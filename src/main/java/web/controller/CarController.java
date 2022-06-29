2e4f6c0c4a9aae1156a644c69a747281381714ab
YuriGorbunov <gorbunov.yuri.n@yandex.ru>
2022-06-29 11:05:42 +0300

    version 1.1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,28 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.models.Car;
 import web.service.CarService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Controller
 public class CarController {
 
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String carList (@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         model.addAttribute("cars", "This is my list of cars");
-        List <Car> list = new ArrayList<>();
-        list.add(new Car("AUDI", "Black", 2010));
-        list.add(new Car("BMW", "Red", 2015));
-        list.add(new Car("Rower", "White", 2019));
-        list.add(new Car("Chevrolet", "Yellow", 2008));
-        list.add(new Car("Lambo", "Orange", 2012));
-        list = CarService.carsCount(list, count);
-        model.addAttribute("list", list);
+        model.addAttribute("list", carService.countCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,15 +1,37 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.models.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CarService {
-    public static List<Car> carsCount(List<Car> list, int limit) {
-        if (limit == 0 || limit == 5) {
-            return list;
-        }
-        return list.stream().limit(limit).collect(Collectors.toList());
+@Service
+public class CarService implements CarServiceInt {
+
+    private List<Car> carList = new ArrayList<>();
+
+    @Override
+    public List<Car> getAllCars() {
+        carList.add(new Car("AUDI", "Black", 2005));
+        carList.add(new Car("BMW", "Orange", 2013));
+        carList.add(new Car("Rower", "White", 2015));
+        carList.add(new Car("Chevrolet", "Yellow", 2022));
+        carList.add(new Car("Lambo", "Red", 2012));
+        return carList;
     }
+    @Override
+    public List<Car> countCars(int limit) {
+        carList = getAllCars();
+        if (limit == 0 || limit > 5) {
+            return carList;
+        }
+        if (limit < 0) {
+            return null;
+        }
+            return carList.stream().limit(limit).collect(Collectors.toList());
+    }
+
+
 }
